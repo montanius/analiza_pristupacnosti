1ef4c5ae61b2a396bceeb37dc5ded280865a9a16
montanius <gmacanovic@gmail.com>
2024-09-06 14:19:13 +0200

    Proširen interface Musterija. Proširene klase musterija i musterijaSaKarticomBiblioteke. Ažurirane metode u klasi biblioteka.

--- biblioteka.js
+++ biblioteka.js
@@ -1,29 +1,102 @@
-var Knjiga = /** @class */ (function () {
-    function Knjiga(autor, naslov, godinaIzdanja, cijena, kolicina) {
-        this.naslov = naslov;
-        this.autor = autor;
-        this.godinaIzdanja = godinaIzdanja;
-        this.cijena = cijena;
-        this.kolicina = kolicina;
+class MusterijaSaKarticomBiblioteke {
+    constructor(musterija, popust) {
+        this.ime = musterija.ime;
+        this.prezime = musterija.prezime;
+        this.godine = musterija.godine;
+        this.brojIznajmljenihKnjiga = musterija.brojIznajmljenihKnjiga;
+        this.stanjeRacuna = musterija.stanjeRacuna;
+        this.iznajmljeneKnjige = musterija.iznajmljeneKnjige;
+        this.popust = popust;
     }
-    Knjiga.prototype.prikaziKnjigu = function () {
-        console.log(this.autor, "\n", this.naslov, "\n", this.godinaIzdanja, "\n", this.cijena, "\n", this.kolicina);
-    };
-    return Knjiga;
-}());
-var Biblioteka = /** @class */ (function () {
-    function Biblioteka() {
+    iznajmiKnjigu(biblioteka, id) {
+        const knjiga = biblioteka.knjige.find(knjiga => knjiga.id === id);
+        if (knjiga) {
+            const cijenaSaPopustom = knjiga.cijena * this.popust;
+            if (knjiga && this.stanjeRacuna < cijenaSaPopustom) {
+                console.log("Korisnik nema dovoljno  sredstava da plati iznajmljivanje  knjige.");
+                return false;
+            }
+            const knjigaIznajmljena = biblioteka.iznajmiKnjigu(id);
+            if (knjiga && this.stanjeRacuna >= cijenaSaPopustom && knjigaIznajmljena) {
+                this.brojIznajmljenihKnjiga += 1;
+                this.stanjeRacuna -= cijenaSaPopustom;
+                this.iznajmljeneKnjige.push(knjiga.id);
+                console.log("provjera: ", this.iznajmljeneKnjige);
+                console.log("Cijena iznajmljivanja knjige je:", cijenaSaPopustom, "€. \n");
+                console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ", this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna, "€");
+            }
+        }
+    }
+    vratiKnjigu(biblioteka, id) {
+        const index = this.iznajmljeneKnjige.indexOf(id);
+        if (index === -1) {
+            console.log(this.ime, " nije iznajmio ovu knjigu.");
+        }
+        else {
+            biblioteka.vratiKnjigu(id);
+            this.brojIznajmljenihKnjiga -= 1;
+            console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ", this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna, "€");
+        }
+    }
+    dopunaRacuna(iznos) {
+        this.stanjeRacuna = this.stanjeRacuna += iznos;
+        console.log("Račun je dopunjen sa", iznos, "€. Stanje na računu je:", this.stanjeRacuna, "€.");
+    }
+}
+class NovaMusterija {
+    constructor(musterija) {
+        this.ime = musterija.ime;
+        this.prezime = musterija.prezime;
+        this.godine = musterija.godine;
+        this.brojIznajmljenihKnjiga = musterija.brojIznajmljenihKnjiga;
+        this.stanjeRacuna = musterija.stanjeRacuna;
+        this.iznajmljeneKnjige = musterija.iznajmljeneKnjige;
+    }
+    iznajmiKnjigu(biblioteka, id) {
+        const knjiga = biblioteka.knjige.find(knjiga => knjiga.id === id);
+        if (knjiga && this.stanjeRacuna < knjiga.cijena) {
+            console.log("Korisnik nema dovoljno  sredstava da plati iznajmljivanje  knjige.");
+        }
+        const knjigaIznajmljena = biblioteka.iznajmiKnjigu(id);
+        if (knjiga && this.stanjeRacuna >= knjiga.cijena && knjigaIznajmljena) {
+            this.brojIznajmljenihKnjiga += 1;
+            this.stanjeRacuna -= knjiga.cijena;
+            this.iznajmljeneKnjige.push(knjiga.id);
+            console.log("provjera: ", this.iznajmljeneKnjige);
+            console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ", this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna, "€");
+        }
+    }
+    dopunaRacuna(iznos) {
+        this.stanjeRacuna = this.stanjeRacuna += iznos;
+        console.log("Račun je dopunjen sa", iznos, "€. Stanje na računu je:", this.stanjeRacuna, "€.");
+    }
+}
+class Knjiga {
+    constructor(svojstvaKnjige) {
+        this.id = svojstvaKnjige.id;
+        this.autor = svojstvaKnjige.autor;
+        this.naslov = svojstvaKnjige.naslov;
+        this.godinaIzdanja = svojstvaKnjige.godinaIzdanja;
+        this.cijena = svojstvaKnjige.cijena;
+        this.kolicina = svojstvaKnjige.kolicina;
+    }
+    prikaziKnjigu() {
+        console.log(this.id, "\n", this.autor, "\n", this.naslov, "\n", this.godinaIzdanja, "\n", this.cijena, "\n", this.kolicina);
+    }
+}
+class Biblioteka {
+    constructor() {
         this.knjige = [];
     }
-    Biblioteka.prototype.prikaziKnjige = function () {
+    prikaziKnjige() {
         console.log(this.knjige);
-    };
-    Biblioteka.prototype.dodajKnjige = function (knjiga) {
+    }
+    dodajKnjige(knjiga) {
         this.knjige.push(knjiga);
         console.log("Knjiga", knjiga.naslov, "je dodata u biblioteku. Broj dodatih primjeraka je", knjiga.kolicina);
-    };
-    Biblioteka.prototype.izbaciKnjigu = function (knjiga) {
-        var index = this.knjige.indexOf(knjiga);
+    }
+    izbaciKnjigu(knjiga) {
+        const index = this.knjige.indexOf(knjiga);
         if (index !== -1) {
             this.knjige.splice(index, 1);
             console.log("Svi primjerci knjige", knjiga.naslov, " su izbačeni iz biblioteke.");
@@ -31,45 +104,75 @@ var Biblioteka = /** @class */ (function () {
         else {
             console.log("Knjiga", knjiga.naslov, "ne postoji u biblioteci.");
         }
-    };
-    Biblioteka.prototype.iznajmiKnjigu = function (knjiga) {
-        var index = this.knjige.indexOf(knjiga);
-        if (index !== -1 && knjiga.kolicina > 1) {
-            knjiga.kolicina -= 1;
-            console.log("Knjiga", knjiga.naslov, "je iznajmljena. Broj primjeraka  koji su ostali u biblioteci je:", knjiga.kolicina);
-        }
-        else if (index !== -1 && knjiga.kolicina === 1) {
-            knjiga.kolicina -= 1;
-            console.log("Knjiga", knjiga.naslov, "je iznajmljena. U biblioteci nije ostao nijedan primjerak knjige.");
+    }
+    iznajmiKnjigu(id) {
+        const knjiga = this.knjige.find(knjiga => knjiga.id === id);
+        if (knjiga === undefined) {
+            console.log("Knjiga ne postoji u biblioteci.");
+            return false;
         }
         else {
-            console.log("Knjiga nije dostupna za iznajmljivanje.");
+            const index = this.knjige.indexOf(knjiga);
+            if (index !== -1 && knjiga.kolicina > 1) {
+                knjiga.kolicina -= 1;
+                console.log("Knjiga", knjiga.naslov, "je iznajmljena. Broj primjeraka  koji su ostali u biblioteci je:", knjiga.kolicina);
+            }
+            else if (index !== -1 && knjiga.kolicina === 1) {
+                knjiga.kolicina -= 1;
+                console.log("Knjiga", knjiga.naslov, "je iznajmljena. U biblioteci nije ostao nijedan primjerak knjige.");
+            }
+            else {
+                console.log("Knjiga nije dostupna za iznajmljivanje.");
+                return false;
+            }
         }
-    };
-    Biblioteka.prototype.vratiKnjigu = function (knjiga) {
-        var index = this.knjige.indexOf(knjiga);
-        if (index !== -1 && knjiga.kolicina >= 1) {
-            knjiga.kolicina += 1;
-            console.log("Knjiga", knjiga.naslov, "je vraćena. Broj primjeraka  koji su dostupni u biblioteci je:", knjiga.kolicina);
-        }
-        else if (index !== -1 && knjiga.kolicina === 0) {
-            knjiga.kolicina += 1;
-            console.log("Knjiga", knjiga.naslov, "je vraćena. U biblioteci je dostupan jedan primjerak knjige.");
+        return true;
+    }
+    vratiKnjigu(id) {
+        const knjiga = this.knjige.find(knjiga => knjiga.id === id);
+        if (knjiga === undefined) {
+            console.log("Knjiga ne postoji u biblioteci.");
+            return false;
         }
         else {
-            console.log("Ova knjiga nije iznajmljivana.");
+            const index = this.knjige.indexOf(knjiga);
+            if (index !== -1 && knjiga.kolicina >= 1) {
+                knjiga.kolicina += 1;
+                console.log("Knjiga", knjiga.naslov, "je vraćena. Broj primjeraka  koji su dostupni u biblioteci je:", knjiga.kolicina);
+            }
+            else if (index !== -1 && knjiga.kolicina === 0) {
+                knjiga.kolicina += 1;
+                console.log("Knjiga", knjiga.naslov, "je vraćena. U biblioteci je dostupan jedan primjerak knjige.");
+            }
+            else {
+                console.log("Ova knjiga nije iznajmljivana.");
+            }
         }
-    };
-    return Biblioteka;
-}());
-var knjiga1 = new Knjiga("Luka Grdinić", "Uvod u Java Script", 2023, 10, 2);
+    }
+}
+const knjiga1 = new Knjiga({
+    id: 1,
+    autor: "Luka Grdinić",
+    naslov: "Uvod u java script",
+    godinaIzdanja: 2023,
+    cijena: 10,
+    kolicina: 2
+});
 knjiga1.prikaziKnjigu();
-var knjiga2 = new Knjiga("Petar Petrović Njegoš", "Gorski Vijenac", 2021, 15, 1);
-var biblioteka = new Biblioteka();
+const knjiga2 = new Knjiga({
+    id: 2,
+    autor: "Petar Petrović Njegoš",
+    naslov: "Gorski vijenac",
+    godinaIzdanja: 2021,
+    cijena: 15,
+    kolicina: 1
+});
+//const knjiga3 = new Knjiga();
+const biblioteka = new Biblioteka();
 biblioteka.dodajKnjige(knjiga1);
 biblioteka.dodajKnjige(knjiga2);
 biblioteka.prikaziKnjige();
-biblioteka.iznajmiKnjigu(knjiga1);
+/*biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.vratiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
@@ -80,4 +183,30 @@ biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.izbaciKnjigu(knjiga1);
 biblioteka.prikaziKnjige();
-biblioteka.izbaciKnjigu(knjiga1);
+biblioteka.izbaciKnjigu(knjiga1);*/
+const ognjen = new NovaMusterija({
+    ime: "Ognjen",
+    prezime: "Macanović",
+    godine: 15,
+    brojIznajmljenihKnjiga: 0,
+    stanjeRacuna: 30,
+    iznajmljeneKnjige: []
+});
+ognjen.iznajmiKnjigu(biblioteka, 1);
+ognjen.iznajmiKnjigu(biblioteka, 1);
+ognjen.dopunaRacuna(15);
+const ivana = new MusterijaSaKarticomBiblioteke({
+    ime: "Ivana",
+    prezime: "Ivanović",
+    godine: 45,
+    brojIznajmljenihKnjiga: 0,
+    stanjeRacuna: 20,
+    iznajmljeneKnjige: []
+}, 0.4);
+ivana.iznajmiKnjigu(biblioteka, 2);
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
--- biblioteka.ts
+++ biblioteka.ts
@@ -1,19 +1,139 @@
+type SvojstvaKnjige = {
+    id : number;
+    autor : string;
+    naslov : string;
+godinaIzdanja : number;
+    cijena : number;
+    kolicina : number;
+}
+
+interface Musterija {
+ime : string,
+prezime : string,
+godine : number,
+brojIznajmljenihKnjiga : number,
+stanjeRacuna : number,
+iznajmljeneKnjige : number[]
+}
+
+class MusterijaSaKarticomBiblioteke implements Musterija {
+    ime : string;
+    prezime : string;
+    godine : number;
+    brojIznajmljenihKnjiga : number;
+    stanjeRacuna : number;
+    iznajmljeneKnjige: number[];
+    popust : number;
+
+    constructor (musterija:Musterija, popust : number){
+this.ime = musterija.ime;
+this.prezime = musterija.prezime;
+this.godine = musterija.godine;
+this.brojIznajmljenihKnjiga = musterija.brojIznajmljenihKnjiga;
+this.stanjeRacuna = musterija.stanjeRacuna;
+this.iznajmljeneKnjige = musterija.iznajmljeneKnjige;
+this.popust = popust;
+}
+
+iznajmiKnjigu (biblioteka:Biblioteka, id:number){
+        const knjiga = biblioteka.knjige.find(knjiga => knjiga.id === id);
+
+        if(knjiga){
+            const cijenaSaPopustom = knjiga.cijena * this.popust ;
+
+        if(knjiga && this.stanjeRacuna < cijenaSaPopustom){
+            console.log("Korisnik nema dovoljno  sredstava da plati iznajmljivanje  knjige.");
+            return false;
+            }
+
+            const knjigaIznajmljena = biblioteka.iznajmiKnjigu(id);
+            
+            if(knjiga && this.stanjeRacuna >= cijenaSaPopustom && knjigaIznajmljena){
+        this.brojIznajmljenihKnjiga += 1;
+    this.stanjeRacuna -= cijenaSaPopustom;
+    this.iznajmljeneKnjige.push(knjiga.id);
+console.log("provjera: ", this.iznajmljeneKnjige);
+    console.log("Cijena iznajmljivanja knjige je:", cijenaSaPopustom, "€. \n");
+    console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ",this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna,"€");
+}
+}
+}
+
+vratiKnjigu (biblioteka:Biblioteka, id:number){
+const index = this.iznajmljeneKnjige.indexOf(id);
+
+if(index === -1){
+console.log(this.ime, " nije iznajmio ovu knjigu.");
+}
+else{
+biblioteka.vratiKnjigu(id);
+this.brojIznajmljenihKnjiga -= 1;
+console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ",this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna,"€");
+}
+}
+
+dopunaRacuna(iznos:number){
+this.stanjeRacuna = this.stanjeRacuna += iznos;
+console.log("Račun je dopunjen sa", iznos, "€. Stanje na računu je:", this.stanjeRacuna, "€.");
+}
+}
+class NovaMusterija implements Musterija {
+    ime : string;
+    prezime : string;
+    godine : number;
+    brojIznajmljenihKnjiga : number;
+    stanjeRacuna : number;
+    iznajmljeneKnjige: number[];
+
+    constructor (musterija:Musterija){
+this.ime = musterija.ime;
+this.prezime = musterija.prezime;
+this.godine = musterija.godine;
+this.brojIznajmljenihKnjiga = musterija.brojIznajmljenihKnjiga;
+this.stanjeRacuna = musterija.stanjeRacuna;
+this.iznajmljeneKnjige = musterija.iznajmljeneKnjige;
+}
+
+iznajmiKnjigu (biblioteka:Biblioteka, id:number){
+    const knjiga = biblioteka.knjige.find(knjiga => knjiga.id === id);
+ 
+if(knjiga && this.stanjeRacuna < knjiga.cijena){
+            console.log("Korisnik nema dovoljno  sredstava da plati iznajmljivanje  knjige.");
+            }
+
+const knjigaIznajmljena = biblioteka.iznajmiKnjigu(id);
+            
+if(knjiga && this.stanjeRacuna >= knjiga.cijena && knjigaIznajmljena){
+        this.brojIznajmljenihKnjiga += 1;
+    this.stanjeRacuna -= knjiga.cijena;
+    this.iznajmljeneKnjige.push(knjiga.id);
+    console.log("provjera: ", this.iznajmljeneKnjige);
+    console.log("Broj knjiga koje je", this.ime, this.prezime, " iznajmio: ",this.brojIznajmljenihKnjiga, "\n", "Preostali iznos na računu je ", this.stanjeRacuna,"€");
+}}            
+dopunaRacuna(iznos:number){
+this.stanjeRacuna = this.stanjeRacuna += iznos;
+console.log("Račun je dopunjen sa", iznos, "€. Stanje na računu je:", this.stanjeRacuna, "€.");
+
+}
+}
 class Knjiga {
-        autor : string;
+        id : number;
+    autor : string;
         naslov : string;
     godinaIzdanja : number;
         cijena : number;
         kolicina : number;
-            constructor(autor:string, naslov:string, godinaIzdanja:number,        cijena:number, kolicina:number){
-this.naslov = naslov;
-this.autor = autor;
-this.godinaIzdanja = godinaIzdanja;
-this.cijena = cijena;
-this.kolicina = kolicina;
+            constructor(svojstvaKnjige:SvojstvaKnjige){
+                this.id = svojstvaKnjige.id;
+                this.autor = svojstvaKnjige.autor;
+                this.naslov = svojstvaKnjige.naslov;
+this.godinaIzdanja = svojstvaKnjige.godinaIzdanja;
+this.cijena = svojstvaKnjige.cijena;
+this.kolicina = svojstvaKnjige.kolicina;
 
     }
     prikaziKnjigu(){
-console.log(this.autor, "\n", this.naslov, "\n", this.godinaIzdanja, "\n", this.cijena, "\n", this.kolicina);
+console.log(this.id, "\n", this.autor, "\n", this.naslov, "\n", this.godinaIzdanja, "\n", this.cijena, "\n", this.kolicina);
 }
 }
 
@@ -43,7 +163,13 @@ console.log("Knjiga", knjiga.naslov, "ne postoji u biblioteci.");
 }
 }
 
-iznajmiKnjigu(knjiga : Knjiga){
+iznajmiKnjigu(id : number):boolean{
+const knjiga = this.knjige.find(knjiga => knjiga.id === id);
+if(knjiga === undefined){
+console.log("Knjiga ne postoji u biblioteci.");
+return false;
+
+}else{
 const index = this.knjige.indexOf(knjiga);
 if(index !== -1 && knjiga.kolicina > 1){
 knjiga.kolicina -= 1;
@@ -55,11 +181,19 @@ knjiga.kolicina -= 1;
 }
 else{
 console.log("Knjiga nije dostupna za iznajmljivanje.");
-}
+return false;
+}}
+return true;
 }
 
-vratiKnjigu(knjiga : Knjiga){
-    const index = this.knjige.indexOf(knjiga);
+vratiKnjigu(id : number){
+    const knjiga = this.knjige.find(knjiga => knjiga.id === id);
+    if(knjiga === undefined){
+        console.log("Knjiga ne postoji u biblioteci.");
+        return false;
+    }
+        else{
+        const index = this.knjige.indexOf(knjiga);
     if(index !== -1 && knjiga.kolicina >= 1){
     knjiga.kolicina += 1;
         console.log("Knjiga", knjiga.naslov, "je vraćena. Broj primjeraka  koji su dostupni u biblioteci je:", knjiga.kolicina);
@@ -71,16 +205,33 @@ vratiKnjigu(knjiga : Knjiga){
     else{
     console.log("Ova knjiga nije iznajmljivana.");
 }
-    }
 }
-const knjiga1 = new Knjiga( "Luka Grdinić","Uvod u Java Script", 2023, 10, 2);
+}}
+
+const knjiga1 = new Knjiga({
+id : 1,
+    autor : "Luka Grdinić",
+naslov : "Uvod u java script",
+godinaIzdanja : 2023,
+cijena : 10,
+kolicina : 2
+});
 knjiga1.prikaziKnjigu();
-const knjiga2 = new Knjiga("Petar Petrović Njegoš","Gorski Vijenac", 2021, 15, 1);
+const knjiga2 = new Knjiga({
+    id : 2,
+    autor : "Petar Petrović Njegoš",
+    naslov : "Gorski vijenac",
+    godinaIzdanja : 2021,
+    cijena : 15,
+    kolicina : 1
+});
+
+//const knjiga3 = new Knjiga();
 const biblioteka = new Biblioteka();
 biblioteka.dodajKnjige(knjiga1);
 biblioteka.dodajKnjige(knjiga2);
 biblioteka.prikaziKnjige();
-biblioteka.iznajmiKnjigu(knjiga1);
+/*biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.vratiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
@@ -91,4 +242,35 @@ biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.iznajmiKnjigu(knjiga1);
 biblioteka.izbaciKnjigu(knjiga1);
 biblioteka.prikaziKnjige();
-biblioteka.izbaciKnjigu(knjiga1);
+biblioteka.izbaciKnjigu(knjiga1);*/
+
+const ognjen  = new NovaMusterija({
+    ime : "Ognjen",
+    prezime : "Macanović",
+godine : 15,
+brojIznajmljenihKnjiga : 0,
+stanjeRacuna : 30,
+iznajmljeneKnjige: []
+}); 
+
+ognjen.iznajmiKnjigu(biblioteka, 1);
+ognjen.iznajmiKnjigu(biblioteka, 1);
+ognjen.dopunaRacuna(15);
+const ivana = new MusterijaSaKarticomBiblioteke({
+ime : "Ivana",
+prezime: "Ivanović",
+godine: 45,
+brojIznajmljenihKnjiga: 0,
+stanjeRacuna: 20,
+iznajmljeneKnjige: []},
+0.4
+);
+
+ivana.iznajmiKnjigu(biblioteka, 2);
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
+
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
+ivana.vratiKnjigu(biblioteka, 2);
+ivana.iznajmiKnjigu(biblioteka, 2);
